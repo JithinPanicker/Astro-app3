0808ac7b712e0a8b17aad2760b4886230c0423cd
JithinPanicker <jithinck2000@gmail.com>
2026-04-22 21:13:37 +0530

    v43 to  v44

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "astro-manager-v43";
+const CACHE_NAME = "astro-manager-v44"; // Increment on each deploy
+
 const ASSETS_TO_CACHE = [
     "./",
     "./index.html",
@@ -15,15 +16,47 @@ const ASSETS_TO_CACHE = [
 
 self.addEventListener("install", (event) => {
     self.skipWaiting();
-    event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)));
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    );
 });
+
 self.addEventListener("activate", (event) => {
-    event.waitUntil(caches.keys().then((cacheNames) => Promise.all(cacheNames.map((cache) => { if (cache !== CACHE_NAME) return caches.delete(cache); }))));
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) return caches.delete(cache);
+                })
+            );
+        })
+    );
     self.clients.claim();
 });
+
 self.addEventListener("fetch", (event) => {
-    event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
+    const url = new URL(event.request.url);
+
+    if (event.request.mode === 'navigate') {
+        event.respondWith(
+            fetch(event.request)
+                .then(response => {
+                    const clonedResponse = response.clone();
+                    caches.open(CACHE_NAME).then(cache => cache.put(event.request, clonedResponse));
+                    return response;
+                })
+                .catch(() => caches.match(event.request))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request).then(response => response || fetch(event.request))
+    );
 });
+
 self.addEventListener('message', (event) => {
-    if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+    if (event.data && event.data.type === 'SKIP_WAITING') {
+        self.skipWaiting();
+    }
 });
